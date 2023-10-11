04e1111a503ca6666a74ed5a1a8825087a458269
yangfan36 <yangfan36@xiaomi.com>
2023-10-11 15:22:22 +0800

    fix

--- Jest-Vue/pages/index.vue
+++ Jest-Vue/pages/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author       : your name
+ * @Date         : 2022-03-04 15:10:38
+ * @LastEditTime : 2022-05-25 14:57:54
+ * @FilePath     : /Jest-Vue/pages/index.vue
+ * @developer    : yangfan36
+-->
 <template>
   <div class="container">
     <div>
@@ -6,17 +13,58 @@
       <h2 class="subtitle">Welcome to the View UI + Nuxt.js template</h2>
       <br />
       <SumTimerTool />
-       <br />
+      <br />
       <CustomParentComponent />
-       <br />
-       <FetchsBtnComponent />
+      <br />
+      <FetchsBtnComponent />
     </div>
   </div>
 </template>
 
 <script>
+import Joi from 'joi'
 export default {
   name: 'IndexPage',
+  created() {},
+  async mounted() {
+    const schema = Joi.object({
+      username: Joi.string().alphanum().min(3).max(30).required(),
+
+      password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
+
+      repeat_password: Joi.ref('password'),
+
+      access_token: [Joi.string(), Joi.number()],
+
+      birth_year: Joi.number().integer().min(1900).max(2013),
+
+      email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ['com', 'net'] },
+      }),
+    })
+      .with('username', 'birth_year')
+      .xor('password', 'access_token')
+      .with('password', 'repeat_password')
+
+    schema.validate({ username: 'abc', birth_year: 1994 })
+    // -> { value: { username: 'abc', birth_year: 1994 } }
+
+    // -> { value: {}, error: '"username" is required' }
+
+    // Also -
+
+    try {
+      const value = await schema.validateAsync({
+        username: 'abc',
+        birth_year: 1994,
+      })
+
+      console.log(value,'111')
+    } catch (err) {
+      console.log(err,'---')
+    }
+  },
 }
 </script>
 
@@ -53,6 +101,6 @@ export default {
 }
 
 p {
-  margin:  20px 0;
+  margin: 20px 0;
 }
 </style>
